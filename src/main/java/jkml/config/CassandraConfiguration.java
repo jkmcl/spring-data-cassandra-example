b449d122731fb75f909dbbeebfd1a0923dc0f8a3
jkmcl <jkmcl>
2021-01-24 16:52:43 +0800

    Replaced direct use of driver API

--- src/main/java/jkml/config/CassandraConfiguration.java
+++ src/main/java/jkml/config/CassandraConfiguration.java
@@ -1,6 +1,10 @@
 package jkml.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.SessionFactory;
+import org.springframework.data.cassandra.core.AsyncCassandraTemplate;
+import org.springframework.data.cassandra.core.convert.CassandraConverter;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 import jkml.data.repository.PackageMarker;
@@ -9,4 +13,10 @@ import jkml.data.repository.support.CustomCassandraRepositoryImpl;
 @Configuration
 @EnableCassandraRepositories(repositoryBaseClass = CustomCassandraRepositoryImpl.class, basePackageClasses = PackageMarker.class)
 public class CassandraConfiguration {
+
+	@Bean
+	public AsyncCassandraTemplate asyncCassandraTemplate(SessionFactory sessionFactory, CassandraConverter converter) {
+		return new AsyncCassandraTemplate(sessionFactory, converter);
+	}
+
 }
--- src/main/java/jkml/data/repository/UserRepositoryImpl.java
+++ src/main/java/jkml/data/repository/UserRepositoryImpl.java
@@ -1,38 +1,23 @@
 package jkml.data.repository;
 
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
-
-import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.datastax.oss.driver.api.core.CqlSession;
-import com.datastax.oss.driver.api.core.cql.AsyncResultSet;
-import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import org.springframework.data.cassandra.core.AsyncCassandraOperations;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import jkml.data.entity.User;
 
 public class UserRepositoryImpl implements UserRepositoryCustom {
 
-	private static final String INSERT_CQL = "INSERT INTO user (id, first_name, last_name) VALUES (?, ?, ?)";
-
-	private PreparedStatement insertStmt;
-
 	private final Logger log = LoggerFactory.getLogger(UserRepositoryImpl.class);
 
 	@Autowired
-	private CqlSession session;
-
-	@PostConstruct
-	private void init() {
-		log.info("Creating prepared statement: {}", INSERT_CQL);
-		insertStmt = session.prepare(INSERT_CQL);
-	}
+	private AsyncCassandraOperations operations;
 
 	@Override
 	public void ingest(List<User> users) {
@@ -40,7 +25,7 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
 		AtomicBoolean hasError = new AtomicBoolean(false);
 
 		// Control number of in-flight async inserts using a semaphore
-		int numPermits = 8; // Value of PoolingOptions.DEFAULT_MAX_POOL_LOCAL in old driver
+		int numPermits = 1024; // Default per connection current request limit in DataStax driver
 		Semaphore semaphore = new Semaphore(numPermits);
 
 		for (User user : users) {
@@ -50,15 +35,23 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
 				semaphore.release();
 				break;
 			}
-			CompletableFuture<AsyncResultSet> rsf = session.executeAsync(insertStmt.bind(user.getId(), user.getFirstName(), user.getLastName())).toCompletableFuture();
-			try {
-				rsf.join();
-			} catch (Exception e) {
-				log.error("Error executing asynchronous insertion", e);
-				hasError.set(true);
-			} finally {
-				semaphore.release();
-			}
+
+			operations.insert(user).addCallback(new ListenableFutureCallback<User>() {
+
+				@Override
+				public void onSuccess(User result) {
+					log.debug("Where am I?");
+					semaphore.release();
+				}
+
+				@Override
+				public void onFailure(Throwable ex) {
+					log.error("Error executing asynchronous insertion", ex);
+					hasError.set(false);
+					semaphore.release();
+				}
+
+			});
 		}
 
 		// Wait for all inserts to complete
