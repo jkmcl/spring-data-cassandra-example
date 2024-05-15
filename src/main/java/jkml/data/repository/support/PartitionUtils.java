f3cde6d6dcfe18d2cdb29431fc27cecda390afd8
Michael Lee <jkmcl@users.noreply.github.com>
2024-05-16 02:41:22 +0800

    Optimizations

--- src/main/java/jkml/data/repository/support/PartitionUtils.java
+++ src/main/java/jkml/data/repository/support/PartitionUtils.java
@@ -2,10 +2,10 @@ package jkml.data.repository.support;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.core.annotation.MergedAnnotations;
@@ -28,38 +28,40 @@ public class PartitionUtils {
 		return AnnotationUtils.findAnnotation(idClass, PrimaryKeyClass.class) != null;
 	}
 
+	static Map<String, Field> getPartitionKeyColumns(Class<?> primaryKeyClass) {
+		var fields = new TreeMap<PrimaryKeyColumn, Field>(CassandraPrimaryKeyColumnAnnotationComparator.INSTANCE);
+		for (var field : primaryKeyClass.getDeclaredFields()) {
+			var pk = field.getDeclaredAnnotation(PrimaryKeyColumn.class);
+			if (pk != null && PrimaryKeyType.PARTITIONED.equals(pk.type())) {
+				fields.put(pk, field);
+			}
+		}
+
+		var result = new LinkedHashMap<String, Field>();
+		for (var entry : fields.entrySet()) {
+			result.put(MergedAnnotations.from(entry.getKey()).get(Column.class).getString("value"), entry.getValue());
+		}
+		return result;
+	}
+
 	public static String getPartitionKeyColumnName(Class<?> entityClass) {
 		for (var field : entityClass.getDeclaredFields()) {
-			var annotation = field.getDeclaredAnnotation(PrimaryKey.class);
-			if (annotation != null) {
-				return MergedAnnotations.from(annotation).get(Column.class).getString("value");
+			var pk = field.getDeclaredAnnotation(PrimaryKey.class);
+			if (pk != null) {
+				return MergedAnnotations.from(pk).get(Column.class).getString("value");
 			}
 		}
 		return null;
 	}
 
-	public static Map<String, Field> getPartitionKeyColumns(Class<?> primaryKeyClass) {
-		var columns = new ArrayList<PrimaryKeyColumn>();
-		var fields = new HashMap<PrimaryKeyColumn, Field>();
-		for (var field : primaryKeyClass.getDeclaredFields()) {
-			var annotation = field.getDeclaredAnnotation(PrimaryKeyColumn.class);
-			if (annotation != null && PrimaryKeyType.PARTITIONED.equals(annotation.type())) {
-				fields.put(annotation, field);
-				columns.add(annotation);
-			}
-		}
-
-		columns.sort(CassandraPrimaryKeyColumnAnnotationComparator.INSTANCE);
-
-		var sortedFields = new LinkedHashMap<String, Field>();
-		for (var col : columns) {
-			sortedFields.put(MergedAnnotations.from(col).get(Column.class).getString("value"), fields.get(col));
-		}
-		return sortedFields;
+	public static List<String> getPartitionKeyColumnNames(Map<String, Field> columns) {
+		var result = new ArrayList<String>(columns.size());
+		columns.keySet().forEach(result::add);
+		return List.copyOf(result);
 	}
 
-	public static List<CriteriaDefinition> getCriteriaDefinition(String columnName, Object id) {
-		return List.of(Criteria.where(columnName).is(id));
+	public static CriteriaDefinition getCriteriaDefinition(String columnName, Object id) {
+		return Criteria.where(columnName).is(id);
 	}
 
 	public static List<CriteriaDefinition> getCriteriaDefinitions(Map<String, Field> columns, Object id) {
@@ -69,25 +71,17 @@ public class PartitionUtils {
 			field.setAccessible(true);
 			result.add(Criteria.where(entry.getKey()).is(ReflectionUtils.getField(field, id)));
 		}
-		return result;
+		return List.copyOf(result);
 	}
 
 	public static List<String> getColumnNames(Class<?> idClass, Class<?> entityClass) {
-		var columnNames = new ArrayList<String>();
-		if (isPrimaryKeyClass(idClass)) {
-			getPartitionKeyColumns(idClass).keySet().forEach(columnNames::add);
-		} else {
-			columnNames.add(getPartitionKeyColumnName(entityClass));
-		}
-		return columnNames;
+		return isPrimaryKeyClass(idClass) ? getPartitionKeyColumnNames(getPartitionKeyColumns(idClass))
+				: List.of(getPartitionKeyColumnName(entityClass));
 	}
 
 	public static List<CriteriaDefinition> getCriteriaDefinitions(Object id, Class<?> idClass, Class<?> entityClass) {
-		if (isPrimaryKeyClass(idClass)) {
-			return getCriteriaDefinitions(getPartitionKeyColumns(idClass), id);
-		} else {
-			return getCriteriaDefinition(getPartitionKeyColumnName(entityClass), id);
-		}
+		return isPrimaryKeyClass(idClass) ? getCriteriaDefinitions(getPartitionKeyColumns(idClass), id)
+				: List.of(getCriteriaDefinition(getPartitionKeyColumnName(entityClass), id));
 	}
 
 }
